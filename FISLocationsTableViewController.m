ae7d8936ff51fdc55b1031af83c2df9250f3f7d8
bmanjuu <bettyofung@gmail.com>
2016-06-27 16:56:32 -0400

    completed set up but tests are failing due to unrecognized selector sent to instance error

---
+++ FISLocationsTableViewController.m
@@ -0,0 +1,118 @@
+//
+//  FISLocationsTableViewController.m
+//  locationTrivia-tableviews
+//
+//  Created by Betty Fung on 6/27/16.
+//  Copyright © 2016 Joe Burgess. All rights reserved.
+//
+
+#import "FISLocationsTableViewController.h"
+#import "FISLocation.h"
+#import "FISTriviaTableViewController.h"
+
+@interface FISLocationsTableViewController ()
+
+@end
+
+@implementation FISLocationsTableViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    
+    self.tableView.accessibilityLabel = @"Locations Table";
+    self.tableView.accessibilityIdentifier = @"Locations Table"; 
+    
+    // FISLocation *location = [[FISLocation alloc] init];
+    
+    
+    // Uncomment the following line to preserve selection between presentations.
+    // self.clearsSelectionOnViewWillAppear = NO;
+    
+    // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
+    // self.navigationItem.rightBarButtonItem = self.editButtonItem;
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+#pragma mark - Table view data source
+
+- (NSInteger)numberOfSectionsInTableView:(UITableView *)tableView {
+    return 1;
+}
+
+- (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section {
+    return self.locations.count;
+}
+
+
+- (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath {
+    
+    UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:@"right detail" forIndexPath:indexPath];
+    
+    FISLocation *currentLocation = self.locations[indexPath.row];
+    
+    cell.textLabel.text = currentLocation.name;
+    cell.detailTextLabel.text = [NSString stringWithFormat:@"%lu", currentLocation.trivia.count];
+    
+    return cell;
+}
+
+
+/*
+// Override to support conditional editing of the table view.
+- (BOOL)tableView:(UITableView *)tableView canEditRowAtIndexPath:(NSIndexPath *)indexPath {
+    // Return NO if you do not want the specified item to be editable.
+    return YES;
+}
+*/
+
+/*
+// Override to support editing the table view.
+- (void)tableView:(UITableView *)tableView commitEditingStyle:(UITableViewCellEditingStyle)editingStyle forRowAtIndexPath:(NSIndexPath *)indexPath {
+    if (editingStyle == UITableViewCellEditingStyleDelete) {
+        // Delete the row from the data source
+        [tableView deleteRowsAtIndexPaths:@[indexPath] withRowAnimation:UITableViewRowAnimationFade];
+    } else if (editingStyle == UITableViewCellEditingStyleInsert) {
+        // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
+    }   
+}
+*/
+
+/*
+// Override to support rearranging the table view.
+- (void)tableView:(UITableView *)tableView moveRowAtIndexPath:(NSIndexPath *)fromIndexPath toIndexPath:(NSIndexPath *)toIndexPath {
+}
+*/
+
+/*
+// Override to support conditional rearranging of the table view.
+- (BOOL)tableView:(UITableView *)tableView canMoveRowAtIndexPath:(NSIndexPath *)indexPath {
+    // Return NO if you do not want the item to be re-orderable.
+    return YES;
+}
+*/
+
+
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    
+    FISTriviaTableViewController *triviaVC = segue.destinationViewController;
+    
+    NSIndexPath *selectedRow = [self.tableView indexPathForSelectedRow];
+    FISLocation *selectedLocation = self.locations[selectedRow.row];
+    
+    triviaVC.triviaArray = selectedLocation.trivia; 
+    
+    //get selected row and use selection to get the the trivia info
+    
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+
+
+@end
---
+++ locationTrivia-tableviews/FISTriviaTableViewController.h
@@ -0,0 +1,15 @@
+//
+//  FISTriviaTableViewController.h
+//  locationTrivia-tableviews
+//
+//  Created by Betty Fung on 6/27/16.
+//  Copyright © 2016 Joe Burgess. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface FISTriviaTableViewController : UITableViewController
+
+@property (strong, nonatomic) NSArray *triviaArray;
+
+@end
